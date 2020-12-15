129ffb5c91220fbe90f03e7c70901bb4b9d13bc7
Denis Chenu <denis@sondages.pro>
2020-12-15 17:12:29 +0100

    [fix][security] Better protection of public method

--- answersAsReadonly.php
+++ answersAsReadonly.php
@@ -5,7 +5,7 @@
  * @author Denis Chenu <denis@sondages.pro>
  * @copyright 2018-2020 Denis Chenu <http://www.sondages.pro>
  * @license AGPL v3
- * @version 0.3.2
+ * @version 0.3.3
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE as published by
@@ -38,81 +38,92 @@ class answersAsReadonly extends PluginBase
      */
     public function getPluginTwigPath()
     {
+        if (!$this->getEvent()) {
+            throw new CHttpException(403);
+        }
         $viewPath = dirname(__FILE__)."/views";
         $this->getEvent()->append('add', array($viewPath));
         $this->unsubscribe('getPluginTwigPath');
     }
 
-  /**
-   * Search all input to set it as disabled and add css and js if needed
-   */
-  public function setReadonly()
-  {
-    $oEvent=$this->getEvent();
-    //~ $this->getEvent()->set("text",$this->getEvent()->get("text")." setReadOnly");
-    $aAttributes=QuestionAttribute::model()->getQuestionAttributes($oEvent->get('qid'));
-    if(isset($aAttributes['readonly']) && $aAttributes['readonly'] ) {
+    /**
+    * Search all input to set it as disabled and add css and js if needed
+    */
+    public function setReadonly()
+    {
+        if (!$this->getEvent()) {
+            throw new CHttpException(403);
+        }
+        $oEvent=$this->getEvent();
+        //~ $this->getEvent()->set("text",$this->getEvent()->get("text")." setReadOnly");
+        $aAttributes=QuestionAttribute::model()->getQuestionAttributes($oEvent->get('qid'));
+        if(empty($aAttributes['readonly'])) {
+            return;
+        }
         $aReplacement=array(
             'QID'=>$oEvent->get('qid'),
             'GID'=>$oEvent->get('gid'),
             'SGQ'=>$oEvent->get('surveyId')."X".$oEvent->get('gid')."X".$oEvent->get('qid'),
         );
         $currentReadonly = trim(LimeExpressionManager::ProcessStepString($aAttributes['readonly'],$aReplacement,3,1));
-        if($currentReadonly) { // then not "", 0, "0" … 
-            $answer = $this->getEvent()->get("answers");
-            $answer = str_replace("type=\"text\"","type=\"text\" readonly ",$answer);
-            $answer = str_replace("type='text'","type='text' readonly ",$answer);
-            $answer = str_replace("<textarea","<textarea readonly ",$answer);
-            /* Remove script for upload */
-            if ($oEvent->get('type') == "|" && version_compare(Yii::app()->getConfig('versionnumber'),"3.10.0",">=")) {
-                $answer = preg_replace('#<script(.*?)>(.*?)</script>#is', '', $answer);
-                $sgqa =  $oEvent->get('surveyId')."X".$oEvent->get('gid')."X".$oEvent->get('qid');
-                $currentValue = $_SESSION['survey_'.$oEvent->get('surveyId')][$sgqa];
-                if(!empty($currentValue)) {
-                    $aFiles = @json_decode($currentValue,true);
-                }
-                if(empty($aFiles)) {
-                    $aFiles = array();
-                }
-                foreach($aFiles as $key => $aFile) {
-                    $sFileLink = null;
-                    $sFileDir = null;
-                    if( substr($aFile['filename'], 0, 3) == 'fu_') {
-                        $sFileDir = Yii::app()->getConfig("uploaddir")."/surveys/".$oEvent->get('surveyId')."/files/";
-                    }
-                    if($sFileDir) {
-                        if (is_file($sFileDir.$aFile['filename'])) {
-                            $sFileLink = Yii::app()->createUrl("uploader/run",array('filegetcontents'=>$aFile['filename']));
-                        }
-                    }
-                    $aFiles[$key]['link'] = $sFileLink;
-                    $aFiles[$key]['name'] = CHtml::encode($aFile['name']);
-                    $aFiles[$key]['comment'] = CHtml::encode($aFile['comment']);
-                    $aFiles[$key]['title'] = CHtml::encode($aFile['title']);
-                }
-                $aQuestionsAttributes = QuestionAttribute::model()->getQuestionAttributes($oEvent->get('qid'));
-                $aData = array(
-                    'value' => $currentValue,
-                    'aAttributes' => $aQuestionsAttributes,
-                    'aSurveyInfo'=> getSurveyInfo($oEvent->get('surveyId'), App()->getLanguage()),
-                    'aFiles' => $aFiles,
-                );
-                $this->subscribe('getPluginTwigPath');
-                $htmlExtra = Yii::app()->twigRenderer->renderPartial('./survey/questions/answer/file_upload/answer_readonly_extra.twig', $aData);
-                $answer .= $htmlExtra;
-            }
-            $oEvent->set("answers",$answer);
-            $oEvent->set("class",$this->getEvent()->get("class")." answersasreadonly-attribute");
-            $this->answersAsReadonlyAddScript();
+        if(!$aAttributes) {
+            return;
         }
+        $answer = $this->getEvent()->get("answers");
+        $answer = str_replace("type=\"text\"","type=\"text\" readonly ",$answer);
+        $answer = str_replace("type='text'","type='text' readonly ",$answer);
+        $answer = str_replace("<textarea","<textarea readonly ",$answer);
+        /* Remove script for upload */
+        if ($oEvent->get('type') == "|" && version_compare(Yii::app()->getConfig('versionnumber'),"3.10.0",">=")) {
+            $answer = preg_replace('#<script(.*?)>(.*?)</script>#is', '', $answer);
+            $sgqa =  $oEvent->get('surveyId')."X".$oEvent->get('gid')."X".$oEvent->get('qid');
+            $currentValue = $_SESSION['survey_'.$oEvent->get('surveyId')][$sgqa];
+            if(!empty($currentValue)) {
+                $aFiles = @json_decode($currentValue,true);
+            }
+            if(empty($aFiles)) {
+                $aFiles = array();
+            }
+            foreach($aFiles as $key => $aFile) {
+                $sFileLink = null;
+                $sFileDir = null;
+                if( substr($aFile['filename'], 0, 3) == 'fu_') {
+                    $sFileDir = Yii::app()->getConfig("uploaddir")."/surveys/".$oEvent->get('surveyId')."/files/";
+                }
+                if($sFileDir) {
+                    if (is_file($sFileDir.$aFile['filename'])) {
+                        $sFileLink = Yii::app()->createUrl("uploader/run",array('filegetcontents'=>$aFile['filename']));
+                    }
+                }
+                $aFiles[$key]['link'] = $sFileLink;
+                $aFiles[$key]['name'] = CHtml::encode($aFile['name']);
+                $aFiles[$key]['comment'] = CHtml::encode($aFile['comment']);
+                $aFiles[$key]['title'] = CHtml::encode($aFile['title']);
+            }
+            $aQuestionsAttributes = QuestionAttribute::model()->getQuestionAttributes($oEvent->get('qid'));
+            $aData = array(
+                'value' => $currentValue,
+                'aAttributes' => $aQuestionsAttributes,
+                'aSurveyInfo'=> getSurveyInfo($oEvent->get('surveyId'), App()->getLanguage()),
+                'aFiles' => $aFiles,
+            );
+            $this->subscribe('getPluginTwigPath');
+            $htmlExtra = Yii::app()->twigRenderer->renderPartial('./survey/questions/answer/file_upload/answer_readonly_extra.twig', $aData);
+            $answer .= $htmlExtra;
+        }
+        $oEvent->set("answers",$answer);
+        $oEvent->set("class",$this->getEvent()->get("class")." answersasreadonly-attribute");
+        $this->answersAsReadonlyAddScript();
     }
-  }
 
     /**
     * Adding the attribute in admin part
     */
     public function addReadonlyAttribute()
     {
+        if (!$this->getEvent()) {
+            throw new CHttpException(403);
+        }
         $scriptAttributes = array(
             'readonly' => array(
                 'name'      => 'readonly',
@@ -137,6 +148,7 @@ class answersAsReadonly extends PluginBase
      */
     public function answersAsReadonlyAddScript()
     {
+        /*Leav public accces : no way to broke security */
         /* Quit if is done */
         if(array_key_exists(get_class($this),Yii::app()->getClientScript()->packages)) {
             return;
